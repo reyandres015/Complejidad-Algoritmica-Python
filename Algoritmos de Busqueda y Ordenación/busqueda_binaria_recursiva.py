14d35c9f983a0180e478f7a784f4607ff9edc90f
AndresRey <reyandres015@gmail.com>
2023-08-02 19:00:40 -0500

    Solucionados Problemas al hacer pruebas de escritorio

--- Algoritmos de Busqueda y Ordenación/busqueda_binaria_recursiva.py
+++ Algoritmos de Busqueda y Ordenación/busqueda_binaria_recursiva.py
@@ -1,31 +1,36 @@
 import random, time
 
-def busqueda_binaria(lista, comienzo, final, objetivo):
-    print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final - 1]}')
-    if comienzo > final:
-        return False
+def busqueda_binaria(lista, comienzo, final, objetivo, contador):
+    if comienzo > final or comienzo >= len(lista) or final > len(lista):
+        print(f'Iteraciones (no encontrado): {contador}')
+        return None
+    else: 
+        print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final - 1]}')
+        
 
     medio = (comienzo + final) // 2
 
     if lista[medio] == objetivo:
+        print(f'Iteraciones: {contador}')
         return medio
     elif lista[medio] < objetivo:
-        return busqueda_binaria(lista, medio + 1, final, objetivo)
+        contador+=1
+        return busqueda_binaria(lista, medio + 1, final, objetivo, contador)
     else:
-        return busqueda_binaria(lista, comienzo, medio - 1, objetivo)
+        contador+=1
+        return busqueda_binaria(lista, comienzo, medio - 1, objetivo, contador)
 
 
 if __name__ == '__main__':
     tamano_de_lista = int(input('De que tamano es la lista? '))
     objetivo = int(input('Que numero quieres encontrar? '))
 
-    lista = sorted([random.randint(0, 100) for i in range(tamano_de_lista)])
+    lista = sorted([i for i in range(tamano_de_lista)])
     
     comienzo = time.time()
-    index = busqueda_binaria(lista, 0, len(lista), objetivo)
+    index = busqueda_binaria(lista, 0, len(lista), objetivo, 1)
     final = time.time()
     
-    print(lista)
-    print(f'El elemento {objetivo} {f"esta en la lista en la posición {index}" if index else "no esta"}')
+    print(f'El elemento {objetivo} {f"esta en la lista en la posición {int(index)}" if index != None else "no esta"}')
 
     print(f'Tiempo de ejecucion: {final-comienzo}')
